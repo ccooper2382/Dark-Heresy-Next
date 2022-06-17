f38287edd0dbbdb162bd500bcb8c35779e22d6d2
ccooper2382 <93221305+ccooper2382@users.noreply.github.com>
2022-06-17 15:01:40 -0500

    Add files via upload

--- pages/components/layout.jsx
+++ pages/components/layout.jsx
@@ -1,14 +1,17 @@
 import React, { Fragment } from 'react';
 import MainHeader from "./main-header";
 import Sidenav from "./sidenav";
+import classes from "./layout.module.css"
 
 
 function Layout(props) {
     return (
         <Fragment>
             <MainHeader/>
-            <Sidenav/>
-            <main>{props.children}</main>
+            <article className={classes.container}>
+                <Sidenav />
+                <main className={classes.body}>{props.children}</main>
+            </article>
         </Fragment>
     );
 }
--- pages/components/sidenav.jsx
+++ pages/components/sidenav.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import Link from "next/link";
+import classes from "./sidenav.module.css";
 
 function Sidenav(props) {
     return (
-        <aside>
+        <aside className={classes.sidenav}>
             <nav>
                 <ul>
-                    <Link href="/">Home</Link>
-                    <Link href="/Character-Creation/Career-Paths">Career Paths</Link>
-                    <Link href="/Character-Creation/Homeworld">Homeworld</Link>
-                    <Link href="/Character-Creation/Characteristics">Characteristics</Link>
-                    <Link href="/Character-Creation/Skills">Skills</Link>
-                    <Link href="/Character-Creation/Talents">Talents</Link>
+                    <li><Link href="/">Home</Link></li>
+                    <li><Link href="/Character-Creation/Career-Paths">Career Paths</Link></li>
+                    <li><Link href="/Character-Creation/Homeworld">Homeworld</Link></li>
+                    <li><Link href="/Character-Creation/Characteristics">Characteristics</Link></li>
+                    <li><Link href="/Character-Creation/Skills">Skills</Link></li>
+                   <li><Link href="/Character-Creation/Talents">Talents</Link></li>
                 </ul>
             </nav>
         </aside>
